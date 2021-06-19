913b052b0e0390b7f4d8bd9e7ef5a6f8b79412e3
eluraschi <Eduardoluraschi@gmail.com>
2021-06-19 20:54:18 -0300

    MVC

---
+++ MVCAlcaldia/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAlcaldia
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
