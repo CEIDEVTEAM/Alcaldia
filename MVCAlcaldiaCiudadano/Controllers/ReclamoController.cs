f3171582f319cfdab5102298f0b82d793d591d15
eluraschi <Eduardoluraschi@gmail.com>
2021-07-03 18:51:14 -0300

    create ReclamoController

---
+++ MVCAlcaldiaCiudadano/Controllers/ReclamoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAlcaldiaCiudadano.Controllers
+{
+    public class ReclamoController : Controller
+    {
+        // GET: Reclamo
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
